f52279a76376b96b22f714f4b822a4a895e7a63f
nayem-1145 <nayem1145@gmail.com>
2025-05-03 14:20:48 +0600

    updated brand slider

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,44 @@
 (function($){
 
 
-    var swiper = new Swiper(".ip_hero_slider", {
+    var heroSlider = new Swiper(".ip_hero_slider", {
         effect: "cards",
         grabCursor: true,
         initialSlide: 1,
     });
     
+
+    var SerisSlider = new Swiper(".ip_series_slider.slider_ltr", {
+        slidesPerView: 'auto',
+        spaceBetween: 24,
+        loop: true,
+        speed: 3000,
+        autoplay: {
+            delay: 0,
+            enabled: true,
+        }
+    });
+
+    var SeriesSliderRtl = new Swiper(".ip_series_slider.slider_rtl", {
+        slidesPerView: 'auto',
+        spaceBetween: 24,
+        loop: true,
+        speed: 3000,
+        autoplay: {
+            delay: 0,
+            enabled: true,
+        }
+    });
+    var BrandSlider = new Swiper(".ip_brand_slider", {
+        slidesPerView: 'auto',
+        spaceBetween: 32,
+        loop: true,
+        speed: 3000,
+        autoplay: {
+            delay: 0,
+            enabled: true,
+        }
+    });
 
 
 
