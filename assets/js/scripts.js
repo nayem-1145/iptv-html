71f5752cc53173c6e69c2145235613d31e1a7990
nayem-1145 <nayem1145@gmail.com>
2025-05-31 18:36:45 +0600

    updated url

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
-(function($){
+(function ($) {
 
 
     var heroSlider = new Swiper(".ip_hero_slider", {
@@ -6,7 +6,7 @@
         grabCursor: true,
         initialSlide: 1,
     });
-    
+
 
     var SerisSlider = new Swiper(".ip_series_slider.slider_ltr", {
         slidesPerView: 'auto',
@@ -39,6 +39,53 @@
             enabled: true,
         }
     });
+
+    var PricingSlider = new Swiper(".ip_pricing_slider", {
+        slidesPerView: 5,
+        spaceBetween: 32,
+        loop: true,
+        speed: 3000,
+        centeredSlides: true,
+        slidesPerGroup: 2,
+        pagination: {
+            el: '.ip_slider_pagination',
+            clickable: true,
+        },
+    });
+
+
+
+
+    var testimonialSlider = new Swiper(".ip_testimonial_slider", {
+        effect: "cards",
+        
+        grabCursor: true,
+        initialSlide: 2,
+
+        cardsEffect: {
+            perSlideOffset: 10,
+            perSlideRotate: 0,
+            slideShadows: false,
+        },
+    });
+
+
+    var postFeaturedSlider = new Swiper(".ip_featured_slider", {
+        slidesPerView: 1,
+        spaceBetween: 15,
+        speed: 1000,
+        autoplay: {
+            delay: 3000,
+            enabled: true,
+            
+        },
+
+        pagination: {
+            el: '.ip_slider_pagination',
+            clickable: true,
+        },
+    });
+
 
 
 
